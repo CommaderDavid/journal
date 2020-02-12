246dfc43c65ac9f09af1cbdbcc0bc7dc04877c83
David Boedigheimer <stealth259@gmail.com>
2020-02-12 15:47:11 -0800

    Add start of main and journal js.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import { journalEntry } from './journal.js';
+
+$(document).ready(function() {
+  $("#new-entry-form").submit(function(e) {
+    e.preventDefault();
+    var title = $("input#new-title").val();
+    var entry = $("input#new-entry").val();
+    
+  })
+});
