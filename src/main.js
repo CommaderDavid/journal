8bb0f2cb50aad7f92f526878218a5f4c61533f8c
David Boedigheimer <stealth259@gmail.com>
2020-02-17 16:05:05 -0800

    Add Display for entries and total word count.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import { journalEntry } from './journal.js';
 $(document).ready(function() {
   $("#new-entry-form").submit(function(e) {
     e.preventDefault();
-    // var title = $("input#new-title").val();
-    var entry = new journalEntry($("input#new-entry").val());
+    var title = $("input#new-title").val();
+    var totalWord = new journalEntry($("input#new-entry").val());
+    var entry = $("input#new-entry").val();
 
-    $("#journal-entries").append(entry.paragraph.length);
+    $("#journal-entries").prepend("<li>" + title + "<br>" + entry + "<br>" + totalWord.paragraph.length + "</li>");
   });
 });
