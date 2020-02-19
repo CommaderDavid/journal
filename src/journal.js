ec6e2c6f4850c65309bdfe6e0bf54045852b0c12
David Boedigheimer <stealth259@gmail.com>
2020-02-19 09:58:42 -0800

    Add if statement and .join to cycle through each letter and avoid spaces.

--- src/journal.js
+++ src/journal.js
@@ -1,27 +1,31 @@
 export function JournalEntry(entry) {
   this.paragraph = entry.split(" ");
-  this.vowels = ['a', 'e', 'i', 'o', 'u'];
-  this.letters = entry.split("");
+  this.letters = this.paragraph.join('').split("");
+
   this.currentVowel = 0;
   this.currentConson = 0;
 }
 
 JournalEntry.prototype.countVowel = function() {
   var allLetters = this.letters;
-  var totalVowel = this.currentVowel;
-  var totalConson = this.currentConson;
+  var totalVowel = 0;
+  var totalConson = 0;
 
   for (var i = 0; i < allLetters.length; i++) {
     // Create if statment that goes through each vowel and adds one to totalVowel
-    //  inside this forEach loop the definition of `this` changes
-    this.vowels.forEach(function(vowel) {
-      if (allLetters[i] === vowel) {
-        totalVowel += 1;
-      } else {
-        totalConson += 1;
-      }
-    });
-    //  `this` goes back to normal
+    if (allLetters[i] === "a") {
+      totalVowel += 1;
+    } else if (allLetters[i] === "e") {
+      totalVowel += 1;
+    } else if (allLetters[i] === "i") {
+      totalVowel += 1;
+    } else if (allLetters[i] === "o") {
+      totalVowel += 1;
+    } else if (allLetters[i] === "u") {
+      totalVowel += 1;
+    } else {
+      totalConson += 1;
+    }
 
     this.currentVowel = totalVowel;
     this.currentConson = totalConson;
